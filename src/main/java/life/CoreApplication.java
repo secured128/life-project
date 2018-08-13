158241183498dd8f1d5ea0037cff367425b273de
Alexander Bromberg <alexander.bromberg@gmail.com>
2018-08-13 10:41:39 +0300

    + Vaadin UI

--- src/main/java/life/CoreApplication.java
+++ src/main/java/life/CoreApplication.java
@@ -1,9 +1,16 @@
 package life;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class CoreApplication {
@@ -15,6 +22,28 @@ public class CoreApplication {
         logger.info("******************************************************************");
         logger.info("****************Life is Up and Running************");
         logger.info("******************************************************************");
+    }
+
+    @Service
+    public static class MyService {
+        public String sayHi() {
+            return "Hello Spring Initializr!";
+        }
+
+    }
+
+    @Theme("valo")
+    @SpringUI(path = "")
+    public static class VaadinUI extends UI {
+
+        @Autowired
+        MyService myService;
+
+        @Override
+        protected void init(VaadinRequest request) {
+            setContent(new Label(myService.sayHi()));
+        }
+
     }
 
 }
