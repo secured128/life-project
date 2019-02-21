e3e3dd7fef311573428ae5dea266ced0f4edda8e
Alexander Bromberg <alexander.bromberg@gmail.com>
2019-02-21 21:47:12 +0200

    Look and say

---
+++ src/main/java/interview/treetraverse/TreeTraverse.java
@@ -0,0 +1,128 @@
+package interview.treetraverse;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeTraverse {
+
+    public static void main(String[] args) {
+        Node n10 = new Node(10);
+        Node n5 = new Node(5);
+        Node n4 = new Node(4);
+        Node n6 = new Node(6);
+        Node n11 = new Node(11);
+        Node n15 = new Node(15);
+        Node n14 = new Node(14);
+
+        n5.setLeft(n4);
+        n5.setRight(n6);
+        n10.setLeft(n5);
+
+        n15.setLeft(n14);
+        n11.setRight(n15);
+        n10.setRight(n11);
+
+        Queue<Node> q = new LinkedList<Node>();
+        ((LinkedList<Node>) q).add(n10);
+
+        bfs(q);
+        System.out.println();
+        dfsPreorder(n10);
+        System.out.println();
+        dfsInorder(n10);
+        System.out.println();
+        dfsPostorder(n10);
+    }
+
+    public static void bfs(Queue<Node> q) {
+        if (q == null || q.isEmpty()) {
+            return;
+        } else {
+            Node node = q.poll();
+            System.out.print(node.getKey() + " ");
+            if (node.getLeft() != null)
+                q.add(node.getLeft());
+            if (node.getRight() != null)
+                q.add(node.getRight());
+
+            bfs(q);
+        }
+    }
+
+    public static void dfsPreorder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            System.out.print(node.getKey() + " ");
+            if (node.getLeft() != null)
+                dfsPreorder(node.getLeft());
+            if (node.getRight() != null)
+                dfsPreorder(node.getRight());
+        }
+    }
+
+
+    public static void dfsInorder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+
+            if (node.getLeft() != null)
+                dfsInorder(node.getLeft());
+            System.out.print(node.getKey() + " ");
+            if (node.getRight() != null)
+                dfsInorder(node.getRight());
+        }
+    }
+
+
+    public static void dfsPostorder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            if (node.getLeft() != null)
+                dfsPostorder(node.getLeft());
+            if (node.getRight() != null)
+                dfsPostorder(node.getRight());
+            System.out.print(node.getKey() + " ");
+        }
+    }
+
+    public static class Node {
+
+        private int key;
+        private Node left;
+        private Node right;
+
+        public Node(int key) {
+            this.key = key;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+
+    }
+
+}
