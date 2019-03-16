4bc7f7e8fdbbfa86a616f97dbdc72cc779f47b37
i350350 <Pojhi9078>
2019-03-16 21:10:06 +0200

    wicket version

--- src/main/java/life/CoreApplication.java
+++ src/main/java/life/CoreApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CoreApplication {
@@ -11,7 +12,8 @@ public class CoreApplication {
     private static final Logger logger = LoggerFactory.getLogger(CoreApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        new SpringApplicationBuilder().sources(CoreApplication.class).run(args);
+//        SpringApplication.run(CoreApplication.class, args);
         logger.info("******************************************************************");
         logger.info("****************Life is Up and Running************");
         logger.info("******************************************************************");
---
+++ src/main/java/life/ui/HomePage.java
@@ -0,0 +1,9 @@
+package life.ui;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+
+@WicketHomePage
+public class HomePage extends WebPage {
+}
+
